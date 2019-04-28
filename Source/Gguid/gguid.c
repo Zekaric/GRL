0ab7f2f4f2b7702dd32afe9de75b54d53e05df54
Robbert de Groot <zekaric@yahoo.ca>
2019-04-28 12:23:33 -0700

    Cleaning up some code.  Gid and Gguid.

--- Source/Gguid/gguid.c
+++ Source/Gguid/gguid.c
@@ -1,7 +1,11 @@
 /******************************************************************************
-file:       grl
+file:       Gguid
 author:     Robbert de Groot
 copyright:  2000-2012, Robbert de Groot
+
+description:
+GUID is the Globally Unique Identifier structure.  This code wraps some of the
+madness.
 ******************************************************************************/
 
 /******************************************************************************
@@ -38,126 +42,100 @@ include:
 #include "pre.h"
 
 /******************************************************************************
-local:
-variable:
-******************************************************************************/
-static Gb     _isStarted = gbFALSE; //lint !e960 !e956
-static Gpath *_exePath   = NULL;    //lint !e960 !e956
-
-/******************************************************************************
 global:
 function:
 ******************************************************************************/
 /******************************************************************************
-func: grlGetExePath
+func: gguidIsEqual
 ******************************************************************************/
-grlAPI Gpath *grlGetExePath(void)
-{
-   genter;
-   greturn _exePath;
-}
-
-/******************************************************************************
-func: grlIsStarted
-******************************************************************************/
-grlAPI Gb grlIsStarted(void)
-{
-   genter;
-   greturn _isStarted;
-}
-
-#if 0
-/******************************************************************************
-func: grlSetExePath
-******************************************************************************/
-grlAPI Gb grlSetExePath(Gpath * const path)
+Gb gguidIsEqual(Gguid const a, Gguid const b)
 {
    genter;
 
-   debugHaltIf(!gpathIsPath(path), "Path is not a Gpath.");
-
-   greturnFalseIf(!gpathIsPath(path));
-   gsSet((Gs *) _exePath, (Gs * const) path);
+   greturnFalseIf(a.n[0] != b.n[0]);
+   greturnFalseIf(a.n[1] != b.n[1]);
 
    greturn gbTRUE;
 }
-#endif
 
+#if 0 //requires gmem
 /******************************************************************************
-func: grlStart
+func: gguidMake
 ******************************************************************************/
-grlAPI Gb grlStart(void)
+Gguid gguidMake(void)
 {
-#if 0
-   returnFalseIf(!gtimeStart());
-   returnFalseIf(!gleakStart());
-   returnFalseIf(!gmemStart());
-#if GTRACE_IS_ON == 1
-   returnFalseIf(!gtraceStartGrl());
+   Gguid guid;
+
+   genter;
+
+#if defined(grlWINDOWS)
+   {
+      GUID newId;
+      CoCreateGuid(&newId);
+
+      guid.b[ 0] = (newId.Data1 >> 24) & 0xFF;
+      guid.b[ 1] = (newId.Data1 >> 16) & 0xFF;
+      guid.b[ 2] = (newId.Data1 >>  8) & 0xFF;
+      guid.b[ 3] = (newId.Data1 >>  0) & 0xFF;
+
+      guid.b[ 4] = (newId.Data2 >>  8) & 0xFF;
+      guid.b[ 5] = (newId.Data2 >>  0) & 0xFF;
+
+      guid.b[ 6] = (newId.Data3 >>  8) & 0xFF;
+      guid.b[ 7] = (newId.Data3 >>  0) & 0xFF;
+
+      gmemCopyOverTypeArray(newId.Data4, Gn1, 8, &(guid.b[8]));
+   }
 #endif
 
-   returnFalseIf(!gtempStart());
-   returnFalseIf(!gcStart());
+#if defined(grlMAX_OSX)
+   {
+      auto newId = CFUUIDCreate(NULL);
+      auto bytes = CFUUIDGetUUIDBytes(newId);
+      CFRelease(newId);
 
-   returnFalseIf(!gprefBinStart());
-
-   returnFalseIf(!gsKeyStart());
-
-   //returnFalseIf(!gpathStart());
-
-   //returnFalseIf(!gpinStart());
-
-   //returnFalseIf(!gregexStart());
-
-   returnFalseIf(!grubStart());
-
-   _exePath   = gsCreate();
+      bytes.byte0,
+         bytes.byte1,
+         bytes.byte2,
+         bytes.byte3,
+         bytes.byte4,
+         bytes.byte5,
+         bytes.byte6,
+         bytes.byte7,
+         bytes.byte8,
+         bytes.byte9,
+         bytes.byte10,
+         bytes.byte11,
+         bytes.byte12,
+         bytes.byte13,
+         bytes.byte14,
+         bytes.byte15
+   }
 #endif
 
-   _isStarted = gbTRUE;
-   return gbTRUE;
+#if defined(grlLINUX)
+   {
+      uuid_t id;
+      uuid_generate(id);
+   }
+#endif
+
+   greturn guid;
 }
 
 /******************************************************************************
-func: grlStop
+func: gguidMakeFromA
 ******************************************************************************/
-grlAPI void grlStop(void)
+Gguid gguidMakeFromA(Char const * const string)
 {
-#if 0
-   gsDestroy(_exePath);
-   _exePath   = NULL;
+   Gguid guid;
 
-   grubStop();
+   genter;
 
-   //gregexStop();
+   string;
 
-   //gpinStop();
+   gmemClearType(&guid, Gguid);
 
-   //gpathStop();
-
-   gsKeyStop();
-
-   gprefBinStop();
-
-   gcStop();//lint !e522
-   gtempStop();
-
-#if GTRACE_IS_ON == 1
-   gtraceStopGrl();
-#endif
-
-   // Ensure the free lists are removed before making the report.
-   gmemFlushPools();
-
-   gmemStop();
-   gleakStop();
-   gtimeStop();
-
-#if GPROFILE_IS_ON == 1
-   gprofileReport();
-#endif
-
-#endif
-
-   _isStarted = gbFALSE;
+   greturn guid;
 }
+#endif
